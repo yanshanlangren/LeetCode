b0f07a862157abc22f0cb1a78012b5e10658bac0
yanshanlangren <elviszhai@yunify.com>
2022-07-27 17:32:11 +0800

    Update TestLinkedBlockingQueue.java

--- src/main/java/elvis/blockingQueue/TestLinkedBlockingQueue.java
+++ src/main/java/elvis/blockingQueue/TestLinkedBlockingQueue.java
@@ -1,35 +1,68 @@
 package elvis.blockingQueue;
 
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.*;
+import java.util.function.Predicate;
 
 public class TestLinkedBlockingQueue extends Thread {
-    BlockingQueue<String> queue = new LinkedBlockingQueue<>();
+    BlockingQueue<Message> queue = new LinkedBlockingQueue<>();
 
     @Override
     public void run() {
         while (true) {
             try {
-                String msg = queue.take();
-                System.out.println("start to deal message[" + msg + "]");
+                Message message = queue.take();
+                System.out.println("start to deal message[" + message.msg + "]...");
                 Thread.sleep(3000);
-                System.out.println("message[" + msg + "] dealt success");
-            } catch (InterruptedException e) {
+//                System.out.println("message[" + message.msg + "] dealt success");
+//                message.p.test("message[" + message.msg + "] dealt");
+                message.c.call();
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
     }
 
-    public void feed(String msg) {
+    public void feed(Message msg) {
         queue.add(msg);
+    }
+
+    public static class Message {
+        String msg;
+        Predicate<String> p;
+        Future<String> f;
+        Callable<String> c;
+
+        public Message(String msg, Predicate<String> p) {
+            this.msg = msg;
+            this.p = p;
+        }
+
+        public Message(String msg, Future<String> f) {
+            this.msg = msg;
+            this.f = f;
+        }
+
+        public Message(String msg, Callable<String> c) {
+            this.msg = msg;
+            this.c = c;
+        }
     }
 
 
     public static void main(String[] args) {
         TestLinkedBlockingQueue t = new TestLinkedBlockingQueue();
         t.start();
-        t.feed("hello");
-        t.feed("my name is elvis");
-        t.feed("see you around");
+        t.feed(new Message("hello", () -> {
+            System.out.println();
+            return "";
+        }));
+//        t.feed(new Message("my name is elvis", x -> {
+//            System.out.println(x);
+//            return true;
+//        }));
+//        t.feed(new Message("see you around", x -> {
+//            System.out.println(x);
+//            return true;
+//        }));
     }
 }
