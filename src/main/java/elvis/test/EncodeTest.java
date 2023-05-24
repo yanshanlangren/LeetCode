ec5e901f123574af4d1cbfafe3f0b580776253aa
yanshanlangren <elviszhai@yunify.com>
2023-05-24 10:42:41 +0800

    update

---
+++ src/main/java/elvis/test/EncodeTest.java
@@ -0,0 +1,8 @@
+package elvis.test;
+
+public class EncodeTest {
+
+    public static void main(String[] args) {
+        System.out.println("你好");
+    }
+}
---
+++ src/main/java/elvis/test/ZKTest.java
@@ -0,0 +1,10 @@
+package elvis.test;
+
+import org.apache.twill.zookeeper.ZKClient;
+
+public class ZKTest {
+
+    public static void main(String[] args) {
+
+    }
+}
