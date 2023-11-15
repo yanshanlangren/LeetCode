3cbcf24fa45ed0efca9548bef682a04e9d31db69
yanshanlangren <elviszhai@yunify.com>
2023-11-15 18:17:35 +0800

    Update LeetCodeCommon1.java

--- src/main/java/elvis/leetcode/LeetCodeCommon1.java
+++ src/main/java/elvis/leetcode/LeetCodeCommon1.java
@@ -268,13 +268,43 @@ public class LeetCodeCommon1 {
         return Math.max(dp[0][len - 1], dp[1][len - 1]);
     }
 
+    /**
+     * https://leetcode.cn/problems/target-sum/
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int findTargetSumWays(int[] nums, int target) {
+        //动态规划
+        //dp[i][j] 表示计算第i个数, 结果为j+1000的结果有多少种
+        //i的数量为nums.length, j的数量为0~2000
+        int len = nums.length;
+        int[][] dp = new int[len][2001];
+        dp[0][1000 + nums[0]] += 1;
+        dp[0][1000 - nums[0]] += 1;
+        int[] sums = new int[nums.length];
+        sums[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            sums[i] = sums[i - 1] + nums[i];
+        }
+        for (int i = 1; i < len; i++) {
+            for (int j = 1000 - sums[i]; j <= 1000 + sums[i]; j++) {
+                if (j - nums[i] >= 0) {
+                    dp[i][j] += dp[i - 1][j - nums[i]];
+                }
+                if (j + nums[i] < 2001) {
+                    dp[i][j] += dp[i - 1][j + nums[i]];
+                }
+            }
+        }
+        return dp[len - 1][target + 1000];
+    }
+
     public static void main(String[] args) {
         LeetCodeCommon1 l = new LeetCodeCommon1();
-        int a = l.wiggleMaxLength(new int[]{33, 53, 12, 64, 50, 41, 45, 21, 97, 35, 47, 92, 39, 0, 93, 55, 40, 46, 69, 42, 6, 95, 51, 68, 72, 9, 32, 84, 34, 64, 6, 2, 26, 98, 3, 43, 30, 60, 3, 68, 82, 9, 97, 19, 27, 98, 99, 4, 30, 96, 37, 9, 78, 43, 64, 4, 65, 30, 84, 90, 87, 64, 18, 50, 60, 1, 40, 32, 48, 50, 76, 100, 57, 29, 63, 53, 46, 57, 93, 98, 42, 80, 82, 9, 41, 55, 69, 84, 82, 79, 30, 79, 18, 97, 67, 23, 52, 38, 74, 15});
-//        int a = l.wiggleMaxLength(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9});
-//        int a = l.wiggleMaxLength(new int[]{1, 17, 5, 10, 13, 15, 10, 5, 16, 8});
-//        int a = l.wiggleMaxLength(new int[]{1, 7, 4, 9, 2, 5});
-//        int a = l.wiggleMaxLength(new int[]{3, 3, 3, 2, 5});
+//        int a = l.findTargetSumWays(new int[]{1, 1, 1, 1, 1}, 3);
+        int a = l.findTargetSumWays(new int[]{0, 0, 0, 0, 0, 0, 0, 0, 1}, 1);
         System.out.println("ans:" + JSONObject.toJSON(a));
     }
 }
